8d2f3c9ed06a81dc0f61415d159b28de0efd442e
sebastian-misiewicz <sebastian@sebastianmisiewicz.info>
2014-08-24 23:27:27 +0200

    Updated Customer

--- sm-evaluation-core/src/main/java/com/sm/evaluation/entity/CustomerBE.java
+++ sm-evaluation-core/src/main/java/com/sm/evaluation/entity/CustomerBE.java
@@ -16,9 +16,39 @@ public class CustomerBE {
 	@Column(name = "id")
 	private Integer id;
 
+	@Column(name = "title", length = 10)
+	private String title;
+
 	@Column(name = "name", nullable = false, length = 30)
 	private String name;
 
+	@Column(name = "password", length = 20)
+	private String password;
+
+	@Column(name = "email", length = 200)
+	private String email;
+	
+	@Column(name = "company", length = 300)
+	private String company;
+	
+	@Column(name = "address", length = 200)
+	private String address;
+	
+	@Column(name = "city", length = 100)
+	private String city;
+	
+	@Column(name = "postalcode", length = 20)
+	private String postalCode;
+	
+	@Column(name = "country", length = 50)
+	private String country;
+	
+	@Column(name = "phone", length = 100)
+	private String phone;
+	
+	@Column(name = "fax", length = 100)
+	private String fax;
+	
 	public Integer getId() {
 		return id;
 	}
