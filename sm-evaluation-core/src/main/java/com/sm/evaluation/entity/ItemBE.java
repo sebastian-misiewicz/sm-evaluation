ac5554f3274d1f202e8bf7a5de1d5b0f44f48c81
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2017-08-27 16:37:17 +0200

    Add item entity

---
+++ sm-evaluation-core/src/main/java/com/sm/evaluation/entity/ItemBE.java
@@ -0,0 +1,53 @@
+package com.sm.evaluation.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item")
+public class ItemBE {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name", length = 200)
+	private String name;
+	
+	@Column(name = "price", columnDefinition = "DECIMAL(7,2)")
+	private BigDecimal price;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	
+
+}
