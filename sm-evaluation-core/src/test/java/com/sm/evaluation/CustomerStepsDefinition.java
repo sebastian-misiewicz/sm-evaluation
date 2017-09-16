cafa20c69c785e407b2abcfd446f49da14c24c37
Sebastian Misiewicz <sebastian@sebastianmisiewicz.info>
2017-09-16 20:33:09 +0200

    Change cucumbe-java8 to cucumber-java and update step definition

--- sm-evaluation-core/src/test/java/com/sm/evaluation/CustomerStepsDefinition.java
+++ sm-evaluation-core/src/test/java/com/sm/evaluation/CustomerStepsDefinition.java
@@ -16,10 +16,10 @@ import com.sm.evaluation.entity.ShoppingCartBE;
 import com.sm.evaluation.entity.ShoppingCartItemBE;
 import com.sm.evaluation.entity.ShoppingCartStatus;
 
-import cucumber.api.java8.En;
+import cucumber.api.java.en.Given;
 
 @ContextConfiguration(classes = {TestContextConfiguration.class})
-public class CustomerStepsDefinition implements En {
+public class CustomerStepsDefinition {
 
 	@Autowired
 	private CustomerDAO customerDAO;
@@ -30,59 +30,63 @@ public class CustomerStepsDefinition implements En {
 	@Autowired
 	private ShoppingCartDAO shoppingCartDAO;
 	
-	public CustomerStepsDefinition() {
-		Given("^Customer with the name '(.+)', e-mail '(.+)'$", (String name, String email) -> {
-		    if (!customerDAO.existsByEmail(email)) {
-		    	CustomerBE customerBE = new CustomerBE();
-		    	customerBE.setEmail(email);
-		    	customerBE.setName(name);
-		    	customerDAO.save(customerBE);
-		    }
-		});
-		
-		Given("^Item '(.+)' with price ([\\d\\.]+)$", (String name, String price) -> {
-			if (!itemDAO.existsByName(name)) {
-				ItemBE itemBE = new ItemBE();
-				itemBE.setName(name);
-				itemBE.setPrice(new BigDecimal(price));
-				
-				itemDAO.save(itemBE);
-			}
-		});
-		
-		Given("^Customer with the email '(.+)' opens a shopping cart '(.+)'$", (String email, String cartHash) -> {
-			if (!shoppingCartDAO.exists(cartHash)) {
-				CustomerBE customer = customerDAO.findOneByEmail(email);
-				ShoppingCartBE shoppingCartBE = new ShoppingCartBE();
-				shoppingCartBE.setHash(cartHash);
-				shoppingCartBE.setCustomer(customer);
-				shoppingCartDAO.save(shoppingCartBE);
-			}
-		});
-
-		Given("^Put '(.+)' into the cart '(.+)' in quantity (\\d+)$", (String name, String cartHash, Integer quantity) -> {
-			ShoppingCartBE shoppingCartBE = shoppingCartDAO.findOne(cartHash);
-			ItemBE itemBE = itemDAO.findOneByName(name);
+	
+	@Given("^Customer with the name '(.+)', e-mail '(.+)'$")
+	public void createCustomer(String name, String email) {
+		if (!customerDAO.existsByEmail(email)) {
+	    	CustomerBE customerBE = new CustomerBE();
+	    	customerBE.setEmail(email);
+	    	customerBE.setName(name);
+	    	customerDAO.save(customerBE);
+	    }
+	}
+	
+	@Given("^Item '(.+)' with price ([\\d\\.]+)$")
+	public void createItem(String name, String price) {
+		if (!itemDAO.existsByName(name)) {
+			ItemBE itemBE = new ItemBE();
+			itemBE.setName(name);
+			itemBE.setPrice(new BigDecimal(price));
 			
-			if (!shoppingCartBE.getItems().stream().anyMatch(shoppingCartItem -> shoppingCartItem.getItem().getName().equals(name))) {
-				ShoppingCartItemBE shoppingCartItemBE = new ShoppingCartItemBE();
-				shoppingCartItemBE.setItem(itemBE);
-				shoppingCartItemBE.setQuantity(quantity);
-				shoppingCartBE.addItem(shoppingCartItemBE);
-				shoppingCartDAO.save(shoppingCartBE);
-			};
-		});
+			itemDAO.save(itemBE);
+		}
+	}
+	
+	@Given("^Customer with the email '(.+)' opens a shopping cart '(.+)'$")
+	public void openShoppingCart(String email, String cartHash) {
+		if (!shoppingCartDAO.exists(cartHash)) {
+			CustomerBE customer = customerDAO.findOneByEmail(email);
+			ShoppingCartBE shoppingCartBE = new ShoppingCartBE();
+			shoppingCartBE.setHash(cartHash);
+			shoppingCartBE.setCustomer(customer);
+			shoppingCartDAO.save(shoppingCartBE);
+		}
+	}
+	
+	@Given("^Put '(.+)' into the cart '(.+)' in quantity (\\d+)$")
+	public void putItemIntoShoppingCart(String name, String cartHash, Integer quantity) {
+		ShoppingCartBE shoppingCartBE = shoppingCartDAO.findOne(cartHash);
+		ItemBE itemBE = itemDAO.findOneByName(name);
 		
-		Given("^Checkout the shopping cart '(.+)' with the date '(.+)'$", (String cartHash, String date) -> {
-			ShoppingCartBE shoppingCartBE = shoppingCartDAO.findOne(cartHash);
-			if (shoppingCartBE != null) {
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d HH:mm:ss");
-				LocalDateTime localDate = LocalDateTime.parse(date, formatter);
-				shoppingCartBE.setCheckoutDate(localDate);
-				shoppingCartBE.setStatus(ShoppingCartStatus.PAID);
-				shoppingCartDAO.save(shoppingCartBE);
-			}
-		});
+		if (!shoppingCartBE.getItems().stream().anyMatch(shoppingCartItem -> shoppingCartItem.getItem().getName().equals(name))) {
+			ShoppingCartItemBE shoppingCartItemBE = new ShoppingCartItemBE();
+			shoppingCartItemBE.setItem(itemBE);
+			shoppingCartItemBE.setQuantity(quantity);
+			shoppingCartBE.addItem(shoppingCartItemBE);
+			shoppingCartDAO.save(shoppingCartBE);
+		};
+	}
+	
+	@Given("^Checkout the shopping cart '(.+)' with the date '(.+)'$")
+	public void checkout(String cartHash, String date) {
+		ShoppingCartBE shoppingCartBE = shoppingCartDAO.findOne(cartHash);
+		if (shoppingCartBE != null) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d HH:mm:ss");
+			LocalDateTime localDate = LocalDateTime.parse(date, formatter);
+			shoppingCartBE.setCheckoutDate(localDate);
+			shoppingCartBE.setStatus(ShoppingCartStatus.PAID);
+			shoppingCartDAO.save(shoppingCartBE);
+		}
 	}
 	
 }
